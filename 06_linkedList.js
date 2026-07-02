a09fea09624e1b6685bdc653bdff08ecc9f5af34
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-02 08:24:16 +0530

    Delete the Node from the NodeList

--- 06_linkedList.js
+++ 06_linkedList.js
@@ -37,40 +37,54 @@ function addToTail(value) {
   this.size++;
 }
 
+//Adding Node at the Index
 
-//Adding Node at the Index 
+function addAtIndex(index, value) {
+  if (index < 0 || index > this.size) return;
+  let newNode = new Node(value);
 
-
-function addAtIndex(index,value){
-  let newNode=new Node(value);
-
-  if(index==0){
+  if (index == 0) {
     this.addToHead(value);
     return;
-  }else if(index==this.size){
+  } else if (index == this.size) {
     this.addToTail(value);
     return;
-  }else {
-    let curr=this.head;
-    for(let i=0;i<index-1;i++){
-      curr=curr.next;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
     }
-    newNode.next=curr.next;
-    curr.next=newNode;
+    newNode.next = curr.next;
+    curr.next = newNode;
   }
   this.size++;
 }
 
-//Get the value from the NodeList 
+//Get the value from the NodeList
 
-function get(index){
-  if(index < 0 || index>=size){
+function get(index) {
+  if (index < 0 || index >= this.size) {
     return -1;
   }
-  let curr=this.head;
-  for(let i=0;i<index;i++){
-    curr=curr.next;
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
   }
-  return curr.value
+  return curr.value;
+}
 
+//Delete the Node from the NodeList
+
+function deleteNode(index) {
+  if (index < 0 || index >= this.size) return;
+  if (index == 0) {
+    this.head = this.head.next;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr=curr.next;
+    }
+    curr.next=curr.next.next;
+  }
+  this.size--;
 }
