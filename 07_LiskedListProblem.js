10d7b2f09df6e8cce6c98db7e22b9cbf2ee82dc3
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-09 11:02:05 +0530

    Remove Duplicates from Linked List

--- 07_LiskedListProblem.js
+++ 07_LiskedListProblem.js
@@ -47,7 +47,7 @@ function linkedCycle(head) {
 //time:=O(n);
 //Space:=O(1);
 function linkedOp(head) {
-  let slow = fast = head;
+  let slow = (fast = head);
   while (fast != null && fast.next != null) {
     slow = slow.next;
     fast = fast.next.next;
@@ -58,112 +58,107 @@ function linkedOp(head) {
   return false;
 }
 
-
 //Linked List Palindrome by converting linkedlist into arr then check
 
-function linkedListPalindrome(head){
-  let arr=[];
-  let curr=head;
-  while(curr){
+function linkedListPalindrome(head) {
+  let arr = [];
+  let curr = head;
+  while (curr) {
     arr.push(curr.value);
-    curr=curr.next;
+    curr = curr.next;
   }
-  let start=0;
-  let end=arr.length-1;
-  while(start<end){
-    if(arr[start]!=arr[end]){
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    if (arr[start] != arr[end]) {
       console.log("Not palindrome Linked List");
       return;
     }
     start++;
     end--;
   }
-  console.log("Palindrome Linked List")
+  console.log("Palindrome Linked List");
 }
 
-//Linked List Optimize 
+//Linked List Optimize
 //time:=O(n);
 //space:=O(1);
-function LinkedPalin(head){
+function LinkedPalin(head) {
   //Find the middle of element
 
-  let slow=fast=head;
-  while(fast!=null && fast.next!=null){
-   slow=slow.next;
-   fast=fast.next.next;
+  let slow = (fast = head);
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  
   //reverse the second half of list
-  let prev=null;
-  let curr=slow;
-  while(curr!=null){
-  let temp=curr.next;
-   curr.next=prev;
-   prev=curr;
-   curr=temp
+  let prev = null;
+  let curr = slow;
+  while (curr != null) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
   }
 
   //checking for plaindrome
-  let first=head;
-  let second=prev;
+  let first = head;
+  let second = prev;
 
-  while(second!=null){
-    if(first.value!==second.value){
-     return false
+  while (second != null) {
+    if (first.value !== second.value) {
+      return false;
     }
-    first=first.next;
-    second=second.next;
+    first = first.next;
+    second = second.next;
   }
   return true;
-} 
-
+}
 
 //Intersection of two Linked List
 //Brute force   time:=O(m*n)=O(n^2);
 // space=O(1);
 
 //Opimize time:=O(1);
-function checkedLiskedList(headA,headB){
+function checkedLiskedList(headA, headB) {
   let seenNode = new Set();
 
-  let first=headA;
-  let second=headB;
+  let first = headA;
+  let second = headB;
   //put all nodes of headA inside a Set
-  while(first){
+  while (first) {
     seenNode.add(first);
-    first=first.next;
+    first = first.next;
   }
 
   //Check for each element of HeadB if they are present in HeadA
-  while(second){
-    if(seenNode.has(second)){
+  while (second) {
+    if (seenNode.has(second)) {
       return second;
     }
-    second=second.next
+    second = second.next;
   }
 
   return null;
-  
-} 
-
-//Remove Linked List Element 
-//Time:=O(n);
-function ListNode(value,next){
-  this.value=value;
-  this.next=next;
 }
-function removeLinked(head,value){
-  let sentinel=new ListNode();
-  sentinel.next=head;
-  let prev=sentinel;
-  while(prev && prev.next){
-    if(prev.next.value===value){
-      prev.next=prev.next.next;
-    }else {
-       prev=prev.next;
+
+//Remove Linked List Element
+//Time:=O(n);
+function ListNode(value, next) {
+  this.value = value;
+  this.next = next;
+}
+function removeLinked(head, value) {
+  let sentinel = new ListNode();
+  sentinel.next = head;
+  let prev = sentinel;
+  while (prev && prev.next) {
+    if (prev.next.value === value) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
     }
-   
   }
   return sentinel.next;
 }
@@ -171,38 +166,58 @@ function removeLinked(head,value){
 //Remove nth node from end of the Linked List // Two pass solution/
 //Time complexity:=O(m+n)=O(2n)=O(n);
 //Spcae Complexity=O(1)
-function removeNthNode(head,n){
-  let sentinel=new ListNode();
-  sentinel.next=head;
-  let prev=sentinel;
-  let length=0;
-  while(head){
-    head=head.next;
-    length++; 
+function removeNthNode(head, n) {
+  let sentinel = new ListNode();
+  sentinel.next = head;
+  let prev = sentinel;
+  let length = 0;
+  while (head) {
+    head = head.next;
+    length++;
   }
- // let deletePosition=(length-n)+1;
-  let prevPosition=length-n;
- for(let i=0;i<prevPosition;i++){
-  prev=prev.next;
- }
- prev.next=prev.next.next;
- return sentinel.next;
+  // let deletePosition=(length-n)+1;
+  let prevPosition = length - n;
+  for (let i = 0; i < prevPosition; i++) {
+    prev = prev.next;
+  }
+  prev.next = prev.next.next;
+  return sentinel.next;
 }
 
-//One pass 
+//One pass
 
-function OnePass(head,n){
-  let sentinel=new ListNode();
-  sentinel.next=head;
-  let first=second=sentinel;
-  for(let i=0;i<n;i++){
-    first=first.next;
+function OnePass(head, n) {
+  //create sentinal node to get previous element add before first node
+  let sentinel = new ListNode();
+  sentinel.next = head;
+  let first = (second = sentinel);
+
+  //move first pointer nth step haead
+  for (let i = 0; i < n; i++) {
+    first = first.next;
   }
 
-  while(first && first.next){
-    second=second.next;
-    first=first.next;
+  //move both first and second pointer 1step till first pointer till reach last node or first.next==null;
+  while (first && first.next) {
+    second = second.next;
+    first = first.next;
   }
-  second.next=second.next.next;
+  //Delete the node
+  second.next = second.next.next;
   return sentinel.next;
+}
+
+//Remove Duplicates from Linked List
+//Time:=O(n);
+//Space:=O(1);
+function removeDuplicates(head) {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.val == curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
 }
