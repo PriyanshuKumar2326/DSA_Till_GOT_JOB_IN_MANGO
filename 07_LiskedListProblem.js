76108ad21d691d5dcbd85eef64755c4531d47294
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-12 12:55:19 +0530

    Merge Two Sorted List

--- 07_LiskedListProblem.js
+++ 07_LiskedListProblem.js
@@ -284,3 +284,28 @@ function addTwoLinkedNum(l1, l2) {
 
   return temp.next;
 }
+
+//Merge Two Sorted List 
+//Time:O(n);
+//space:O(1)
+function mergeList(l1,l2){
+  let ans=new ListNode();
+  let curr=ans;
+  while(l1 && l2){
+    if(l1.val<l2.val){
+      curr.next=l1;
+      l1=l1.next;
+    }else{
+      curr.next=l2;
+      l2=l2.next;
+    }
+    curr=curr.next
+  }
+  if(!l1){
+    curr.next=l2
+  }else if(!l2){
+    curr.next=l1
+  }
+  
+  return ans.next
+}
