b34c1b411a1311c2dfc7c53fe889b835f0dd46f9
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-19 09:01:22 +0530

    Largest Odd Number in String

--- 08_String.js
+++ 08_String.js
@@ -219,3 +219,17 @@ function balancedString(string){
   }
   return balanced
 }
+
+
+//Largest Odd Number in String A
+
+function largestOddNumber(num){
+  let n=num.length-1;
+  while(n>=0){
+    if(Number(num[n])%2==1){
+      return num.substring(0,n+1)
+    }
+    --n;
+  }
+  return ""
+}
