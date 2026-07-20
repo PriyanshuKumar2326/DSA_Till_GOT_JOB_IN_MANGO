f3612a1c9fd599c9588383eb77d32f4ee033e48d
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-20 06:55:26 +0530

    Largest Common Prifix

--- 08_String.js
+++ 08_String.js
@@ -232,4 +232,35 @@ function largestOddNumber(num){
     --n;
   }
   return ""
+} 
+
+
+//Split a string in Balanced Strings 
+
+function balancedS(string){
+  let temp=0;
+  let count=0;
+  for(let i=0;i<string.length;i++){
+    {string[i]=="R" ? temp++ :temp--}
+    if(temp==0) count++
+  }
+  return count;
+} 
+
+//Largest Common Prifix 
+//Time:=O(s);
+//space:=O(1)
+
+function commonPrix(strs){
+  let x=0;
+  while(x< strs[0].length){
+    let ch=strs[0][x]
+    for(let i=1;i<strs.length;i++){
+      if(strs[i][x]!==ch || x==strs[i].length){
+        return strs[0].substring(0,x);
+      }
+    }
+    ++x
+  }
+  return strs[0]
 }
