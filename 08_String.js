d7e422d5083f1392303faa3151ca41700890dcad
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-23 06:05:15 +0530

    Group Anagram 2nd Approch

--- 08_String.js
+++ 08_String.js
@@ -314,6 +314,7 @@ function isomorphic(s,t){
 
 //Group Anagram 
 
+//1st Approch
 function groupAnagram(strs){
   
   let map={};
@@ -329,5 +330,32 @@ function groupAnagram(strs){
   return [...Object.values(map)]
   //time:O(n * mlogm);
   //space:O(n*m)
+}
+
+//2nd Approch 
+
+function secondGA(strs){
+  let map={};
+
+  for(let i=0;i<strs.length;i++){
+    let freqArr=Array(26).fill(0);
+    let s=strs[i];
+    for(let j=0;j<s.length;j++){
+      let index=s[j].charCodeAt()-'a'.charCodeAt();
+      ++freqArr[index]
+    }
+    let key="";
+    for(let k=0;k<26;k++){
+      key += String.fromCharCode(97 + k) + freqArr[k];
+    }
+
+    if(!map[key]){
+      map[key]=[s]
+    }else {
+      map[key].push(s);
+    }
+  
+  }
+  return [...Object.values(map)]
 }
 
