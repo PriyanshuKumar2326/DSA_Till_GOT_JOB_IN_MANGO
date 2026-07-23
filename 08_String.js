becb14ffae8d01ad9b55fee58a659455d6e09cf1
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-23 05:31:50 +0530

    Group Anagram First approch

--- 08_String.js
+++ 08_String.js
@@ -311,3 +311,23 @@ function isomorphic(s,t){
   }
   return true;
 }
+
+//Group Anagram 
+
+function groupAnagram(strs){
+  
+  let map={};
+  for(let i=0;i<strs.length;i++){
+    let sortedStr=strs[i].split("").sort().join("");
+    if(!map[sortedStr]){
+      map[sortedStr]=[strs[i]]
+    }else {
+      map[sortedStr].push(strs[i])
+    }
+  }
+
+  return [...Object.values(map)]
+  //time:O(n * mlogm);
+  //space:O(n*m)
+}
+
