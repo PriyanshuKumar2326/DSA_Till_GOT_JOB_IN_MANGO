5f8f346c318129e21183030f845c7c7b989f3118
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-26 05:18:29 +0530

    Implement of Stack and Queue with one Queue

--- 09_Stack_and_Queues.js
+++ 09_Stack_and_Queues.js
@@ -27,49 +27,80 @@ queue.push(4);
 
 
 //Implement of Stack and Queue
-
+//with two queue
 //Create stack 
-var MyStack=function(){
-  this.q1=[];
-  this.q2=[];
-}
+// var MyStack=function(){
+//   this.q1=[];
+//   this.q2=[];
+// }
 
-MyStack.prototype.push=function(x){
-  this.q1.push(x);
-}
+// MyStack.prototype.push=function(x){
+//   this.q1.push(x);
+// }
 
-MyStack.prototype.pop=function(){
-let n=this.q1.length;
-for(let i=0;i<n-1;i++){
-   let frontEle=this.q1.shift();
-   this.q2.push(frontEle);
-}
+// MyStack.prototype.pop=function(){
+// let n=this.q1.length;
+// for(let i=0;i<n-1;i++){
+//    let frontEle=this.q1.shift();
+//    this.q2.push(frontEle);
+// }
 
-let ans= this.q1.shift();
-//exchange 
-let temp=this.q1;
-this.q1=this.q2;
-this.q2=temp;
+// let ans= this.q1.shift();
+// //exchange 
+// let temp=this.q1;
+// this.q1=this.q2;
+// this.q2=temp;
 
-return ans;
+// return ans;
+// } 
+
+// //Top 
+// MyStack.prototype.top=function(){
+//   let n=this.q1.length;
+//   for(let i=0;i<n-1;i++){
+//     this.q2.push(this.q1.shift());
+//   }
+//   let front=this.q1[0];
+//   this.q2.push(this.q1.shift());
+//   let temp=this.q1;
+//   this.q1=this.q2;
+//   this.q1=temp;
+//   return front;
+// } 
+
+// //Empty 
+
+// MyStack.prototype.empty=function(){
+//   return this.q1.length===0
+// }
+//with one Queue 
+
+var MyStack1=function(){
+  this.q=[];
 } 
 
-//Top 
-MyStack.prototype.top=function(){
-  let n=this.q1.length;
+MyStack1.prototype.push=function(x){
+  this.q.push(x)
+}
+
+MyStack1.prototype.pop1=function(){
+  let n=this.q.length;
   for(let i=0;i<n-1;i++){
-    this.q2.push(this.q1.shift());
+    this.q.push(this.q.shift())
   }
-  let front=this.q1[0];
-  this.q2.push(this.q1.shift());
-  let temp=this.q1;
-  this.q1=this.q2;
-  this.q1=temp;
-  return front;
+  return this.q.shift()
 } 
 
-//Empty 
+MyStack1.prototype.top1=function(){
+  let n=this.q.length;
+  for(let i=0;i<n-1;i++){
+    this.q.push(this.q.shift());
+  }
+  let ans = this.q[0];
+  this.q.push(this.q.shift())
+  return ans
+}
 
-MyStack.prototype.empty=function(){
-  return this.q1.length===0
+MyStack1.prototype.empty=function(){
+  return this.q.length===0;
 }
