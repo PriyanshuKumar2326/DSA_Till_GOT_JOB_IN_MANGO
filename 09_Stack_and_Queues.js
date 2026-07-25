12042214cb7bb4ce5755d3eadc7a0bf18ed5f526
Priyanshu Kumar <amul12345amul1@gmail.com>
2026-07-25 06:52:24 +0530

    Implement of Stack and Queue

--- 09_Stack_and_Queues.js
+++ 09_Stack_and_Queues.js
@@ -23,6 +23,53 @@ queue.push(4);
 //console.log(queue)
 //queue.shift(); // dequeue
 //console.log(queue) 
-console.log(queue[0])
+// console.log(queue[0])
 
 
+//Implement of Stack and Queue
+
+//Create stack 
+var MyStack=function(){
+  this.q1=[];
+  this.q2=[];
+}
+
+MyStack.prototype.push=function(x){
+  this.q1.push(x);
+}
+
+MyStack.prototype.pop=function(){
+let n=this.q1.length;
+for(let i=0;i<n-1;i++){
+   let frontEle=this.q1.shift();
+   this.q2.push(frontEle);
+}
+
+let ans= this.q1.shift();
+//exchange 
+let temp=this.q1;
+this.q1=this.q2;
+this.q2=temp;
+
+return ans;
+} 
+
+//Top 
+MyStack.prototype.top=function(){
+  let n=this.q1.length;
+  for(let i=0;i<n-1;i++){
+    this.q2.push(this.q1.shift());
+  }
+  let front=this.q1[0];
+  this.q2.push(this.q1.shift());
+  let temp=this.q1;
+  this.q1=this.q2;
+  this.q1=temp;
+  return front;
+} 
+
+//Empty 
+
+MyStack.prototype.empty=function(){
+  return this.q1.length===0
+}
